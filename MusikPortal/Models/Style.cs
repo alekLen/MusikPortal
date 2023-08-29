fe91e72b60c344800a5576f962a07b6c2daa635c
Elena <aleklenoch@gmail.com>
2023-08-29 12:57:32 +0300

    # в моделях стиляпесен и исполнителей наличие коллекций песен обязательно

--- MusikPortal/Models/Style.cs
+++ MusikPortal/Models/Style.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public ICollection<Song>? Songs { get; set; }
+        public ICollection<Song> Songs { get; set; }=new List<Song>();
     }
 }
