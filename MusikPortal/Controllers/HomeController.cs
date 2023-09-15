a12ef8366aa6fb8a46312856dc54051302d28d7a
Elena <aleklenoch@gmail.com>
2023-09-15 22:57:29 +0300

    #SPA поиск песни и возврат ко всему списку песен

--- MusikPortal/Controllers/HomeController.cs
+++ MusikPortal/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using MusikPortal.Models;
 using MusicPortal.BLL.DTO;
 using MusicPortal.BLL.Interfaces;
 using System.Diagnostics;
+using AutoMapper;
+using Newtonsoft.Json;
 
 namespace MusikPortal.Controllers
 {
@@ -19,11 +21,19 @@ namespace MusikPortal.Controllers
             ViewBag.Songs = s;
             return View();
         }
+        [HttpPost]
         public async Task<IActionResult> Find(string str)
         {
             IEnumerable<SongDTO> s = await songService.FindSongs(str);
-            ViewBag.Songs = s;
-            return View("Index");
+            string response = JsonConvert.SerializeObject(s);
+            return Json(response);
+        }
+        public async Task<IActionResult> AllSongs()
+        {
+            IEnumerable<SongDTO> s = await songService.GetAllSongs();
+            string response = JsonConvert.SerializeObject(s);
+             return Json(response);
+           
         }
     }
 }
